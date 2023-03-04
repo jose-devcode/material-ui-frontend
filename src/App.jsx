e91a6a61ccd67cf3f925beff72ed70a99dcb5b06
Jose Raul <dxt.nis@gmail.com>
2023-03-04 10:52:38 +0000

    contacts done

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import SidebarComponent from './scenes/global/SidebarComponent'
 import Dashboard from './scenes/dashboard'
 import { Routes, Route } from 'react-router-dom'
 
-// import Team from './scenes/team'
-// import Invoices from './scenes/invoices'
-// import Contacts from './scenes/contacts'
+import Team from './scenes/team'
+import Invoices from './scenes/invoices'
+import Contacts from './scenes/contacts'
 // import Bar from './scenes/bar'
 // import Form from './scenes/from'
 // import Line from './scenes/line'
@@ -32,9 +32,9 @@ function App() {
               <Topbar />
               <Routes>
                 <Route path='/' element={<Dashboard />} />
-                {/* <Route path='/team' element={<Team />} /> */}
-                {/* <Route path='/contacts' element={<Contacts />} /> */}
-                {/* <Route path='/invoices' element={<Invoices />} /> */}
+                <Route path='/team' element={<Team />} />
+                <Route path='/contacts' element={<Contacts />} />
+                <Route path='/invoices' element={<Invoices />} />
                 {/* <Route path='/form' element={<Form />} /> */}
                 {/* <Route path='/bar' element={<Bar />} /> */}
                 {/* <Route path='/pie' element={<Pie />} /> */}
